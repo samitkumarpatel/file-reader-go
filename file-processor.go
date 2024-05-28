948f223484905bb0d1663aef3c084bc9d51eed67
samitkumarpatel <samitkumarpatel@gmail.com>
2024-05-28 23:56:15 +0200

    rabbitmq to redis pubsub switch

--- file-processor.go
+++ file-processor.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-    filename := "sample.txt"
-    file, err := os.Open(filename)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer file.Close()
+	filename := "sample.txt"
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    lines, words, letters := 0, 0, 0
-    for scanner.Scan() {
-        lines++
-        words += len(strings.Fields(scanner.Text()))
-        letters += len(scanner.Text())
-    }
+	scanner := bufio.NewScanner(file)
+	lines, words, letters := 0, 0, 0
+	for scanner.Scan() {
+		lines++
+		words += len(strings.Fields(scanner.Text()))
+		letters += len(scanner.Text())
+	}
 
-    fmt.Println("Lines:", lines)
-    fmt.Println("Words:", words)
-    fmt.Println("Letters:", letters)
+	fmt.Println("Lines:", lines)
+	fmt.Println("Words:", words)
+	fmt.Println("Letters:", letters)
 }
